40dc629ca0b53a693223429896f80e236a7117f6
Ben Wander <ben@thewandering.band>
2021-03-21 11:37:40 -0600

    Remove Texture3D cruft

--- Assets/Scripts/NoiseGenerator.cs
+++ Assets/Scripts/NoiseGenerator.cs
@@ -8,18 +8,9 @@ public class NoiseGenerator : MonoBehaviour
     [SerializeField] Vector3 divisions = new Vector3(8, 8, 8);
     [SerializeField] Vector3 divisionsWeight = new Vector3(1f/3f, 1f/3f, 1f/3f);
     [SerializeField] bool generateCenters;
-    [SerializeField] bool showPreview;
-    [SerializeField] Vector3 previewSlices;
-    
-    public Texture3D CurrentTexture3D { get { return currentTexture3D; } }
-    Texture3D currentTexture3D;
     
     public RenderTexture CurrentRenderTexture { get { return renderTexture; } }
     RenderTexture renderTexture;
-    
-    public bool ShowPreview { get { return showPreview; } }
-    public Vector3 PreviewSlices { get { return previewSlices; } }
-
     int textureSize = 256;
     
     public void GenerateNoise()
@@ -44,23 +35,5 @@ public class NoiseGenerator : MonoBehaviour
         computeShader.SetFloat("DivisionsWeight2", divisionsWeight.z / totalWeight);
         computeShader.SetInt("Size", textureSize);
         computeShader.Dispatch(kernelHandle, textureSize/8, textureSize/8, textureSize/8);
-        
-        Texture3D texture3D = new Texture3D(textureSize, textureSize, textureSize, TextureFormat.ARGB32, false);
-        Graphics.CopyTexture(renderTexture, texture3D);
-
-        currentTexture3D = texture3D;
-    }
-
-#if UNITY_EDITOR
-    public void SaveNoise()
-    {
-        if (currentTexture3D != null)
-            UnityEditor.AssetDatabase.CreateAsset(currentTexture3D, "Assets/Worley3DTexture.asset");
-    }
-#endif
-
-    public void ClearNoise()
-    {
-        currentTexture3D = null;
     }
 }
